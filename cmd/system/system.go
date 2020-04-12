f864c373785eddc4888a33d38cea297ef21eb0a0
Erik Sipsma <erik@sipsma.dev>
2020-04-12 16:11:47 -0700

    Add json dump option to system llb generator.
    This is helpful for debugging and getting a better understanding of the LLB
    underlying the build.
    
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- cmd/system/system.go
+++ cmd/system/system.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"io"
 	"os"
 
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/solver/pb"
+	"github.com/opencontainers/go-digest"
 	"github.com/sipsma/bincastle/distro"
 	"github.com/sipsma/bincastle/graph"
 )
+
+var dumpJsonFlag bool
+
+func init() {
+	flag.BoolVar(&dumpJsonFlag, "json", false,
+		"write formatted json instead of marshalled protobuf (for debugging)")
+	flag.Parse()
+}
 
 func main() {
 	dt, err := distro.Bootstrap(graph.Import(
@@ -16,8 +29,41 @@ func main() {
 		panic(err)
 	}
 
-	err = llb.WriteTo(dt, os.Stdout)
+	if dumpJsonFlag {
+		err = dumpJson(dt, os.Stdout)
+	} else {
+		err = llb.WriteTo(dt, os.Stdout)
+	}
 	if err != nil {
 		panic(err)
 	}
+}
+
+func dumpJson(def *llb.Definition, output io.Writer) error {
+	enc := json.NewEncoder(output)
+	enc.SetIndent("", "  ")
+	for _, dt := range def.Def {
+		var op pb.Op
+		err := (&op).Unmarshal(dt)
+		if err != nil {
+			return err
+		}
+
+		dgst := digest.FromBytes(dt)
+		err = enc.Encode(llbOp{
+			Op:         op,
+			Digest:     dgst,
+			OpMetadata: def.Metadata[dgst],
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type llbOp struct {
+	Op         pb.Op
+	Digest     digest.Digest
+	OpMetadata pb.OpMetadata
 }
